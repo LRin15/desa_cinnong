29137c9892382cc1725fa5f4dbdd6afc0e4ad954
LRin15 <kindikindi30@gmail.com>
2025-08-20 13:39:19 +0800

    Memperbarui beranda

--- routes/web.php
+++ routes/web.php
@@ -37,7 +37,7 @@ Route::get('/', function () {
             'tanggal_terbit' => $berita->tanggal_terbit->format('d F Y'),
             'kutipan' => Str::limit($berita->kutipan, 100),
             // 👇 TAMBAHKAN BARIS INI 👇
-            'gambar' => $berita->gambar ? asset( $berita->gambar) : null,
+            'gambar' => $berita->gambar ? Storage::url($berita->gambar) : null,
         ]);
 
     return Inertia::render('Beranda', [
